d1f9fe3e71f56a0e1c9efe27ffe19dee7d90bbcf
rmoreno <rmoreno@saurus.com>
2023-12-20 12:05:50 +0100

    refactored

--- rewire-holding-payment-gateway-plugin/rewireholding-payment-gateway.php
+++ rewire-holding-payment-gateway-plugin/rewireholding-payment-gateway.php
@@ -1,29 +1,27 @@
 <?php
 
 /**
- * Plugin Name: Mi Método de Pago
- * Plugin URI: http://mitienda.com
- * Description: Un método de pago personalizado para WooCommerce.
+ * Plugin Name: Rewire Holding Payment Gateway
+ * Plugin URI: https://rewireholding.com/
+ * Description: Rewire Holding WooCommerce Payment Gateway
  * Version: 1.0
- * Author: Tu Nombre
- * Author URI: http://tusitio.com
+ * Author: Rafael Moreno
  */
 
 if (!defined('ABSPATH')) {
-    exit; // Salir si se accede directamente.
+    exit; 
 }
 
-function init_mi_metodo_pago()
+function init_rewire_holding_payment_gateway()
 {
     if (!class_exists('WC_Payment_Gateway')) return;
 
-    class WC_Mi_Metodo_Pago extends WC_Payment_Gateway
+    class WC_RewireHolding_Payment_Gateway extends WC_Payment_Gateway
     {
         public function __construct()
         {
-            // Configuraciones iniciales del método de pago aquí...
-            $this->id                 = 'mi_pasarela_de_pagos';
-            $this->icon               = apply_filters('woocommerce_mi_pasarela_de_pagos_icon', '');
+            $this->id                 = 'rewireholding_payment_gateway';
+            $this->icon               = apply_filters('woocommerce_payment_gateway_icon', '');
             $this->has_fields         = false;
             $this->method_title       = __('Rewire Holding Payment Gateway', 'woocommerce');
             $this->method_description = __('Allows card payments through the Rewire Holding Payment Gateway, with Visa & Mastercard, Debid and Prepaid Cards', 'text-domain');
@@ -36,7 +34,6 @@ function init_mi_metodo_pago()
 
             add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
 
-            // Cargar configuraciones, acciones, etc.
         }
 
         public function get_option($key, $empty_value = null)
@@ -50,13 +47,12 @@ function init_mi_metodo_pago()
         {
             $order = wc_get_order($order_id);
 
-            // Preparar la URL de redirección con los parámetros necesarios
             $response = $this->api_call($order);
 
             if (is_wp_error($response)) {
                 return array(
                     'result'   => 'failure',
-                    'message' => 'hola'
+                    'message' => ''
                 );
             }
 
@@ -65,27 +61,25 @@ function init_mi_metodo_pago()
 
 
             if (!empty($data->url)) {
-		$order->update_meta_data('payment_api_id',$data->guid);
-		$order->save();
-                // Redirigir a la URL proporcionada por la API
+                $order->update_meta_data('payment_api_id', $data->guid);
+                $order->save();
                 return array(
                     'result'   => 'success',
                     'redirect' => $data->url
                 );
             } else {
-                // Manejar casos donde no se recibe una URL
                 return array(
                     'result'   => 'failure',
-                    'message' => 'Error al obtener la URL de redirección.'
+                    'message' => 'Error getting redirection URL.'
                 );
             }
         }
 
         private function api_call($order)
         {
-            // Configura los parámetros de la API aquí
             $username = $this->get_option('api_key');
             $password = $this->get_option('api_secret');
+            $domain = $this->get_option('domain');
             $api_url = 'https://api.saurus.com/data/api/v1.0/merchant/topup/paymentgateway/api/draft';
             $headers = array(
                 'Authorization' => 'Basic ' . base64_encode($username . ':' . $password),
@@ -98,17 +92,14 @@ function init_mi_metodo_pago()
                 'body' => json_encode(array(
                     'asset_order_id' => $order->get_id(),
                     'amount'   => $order->get_total(),
-                    'callback_url' => 'https://britscope.com/?verificar_pago=1&order=' . $order->get_id()
-                    // Otros datos necesarios para tu API de pago
+                    'callback_url' => $domain.'/?verificar_pago=1&order=' . $order->get_id()
                 )),
                 'timeout' => 45,
             );
 
-            // Realiza la llamada a la API
             $response = wp_remote_post($api_url, $api_args);
             return $response;
         }
-        // Otros métodos necesarios aquí...
         public function admin_options()
         {
             echo '<h2>' . esc_html($this->method_title) . '</h2>';
@@ -122,8 +113,8 @@ function init_mi_metodo_pago()
         {
             $this->form_fields = array(
                 'enabled' => array(
-                    'title'       => 'Habilitar/Deshabilitar',
-                    'label'       => 'Habilitar Mi Método de Pago',
+                    'title'       => 'Enable/Disable',
+                    'label'       => 'Enable Rewire Holding Payment Gateway',
                     'type'        => 'checkbox',
                     'description' => '',
                     'default'     => 'no',
@@ -138,22 +129,29 @@ function init_mi_metodo_pago()
                 'description' => array(
                     'title'       => __('Descripción', 'text-domain'),
                     'type'        => 'textarea',
-                    'description' => __('Esto controla la descripción que el usuario ve durante el checkout.', 'text-domain'),
-                    'default'     => __('Pago a través de Mi Método de Pago.', 'text-domain'),
+                    'description' => __('This controls the description the user sees during checkout.', 'text-domain'),
+                    'default'     => __('Payment via Rewire Holding Payment Gateway.', 'text-domain'),
                     'desc_tip'    => true,
                 ),
                 'api_key' => array(
-                    'title'       => 'Clave API',
+                    'title'       => 'Key API',
                     'type'        => 'text',
-                    'description' => 'Ingresa tu clave de API proporcionada por el servicio de pagos.',
+                    'description' => 'Enter your API key provided by the payment service.',
                     'default'     => '',
                     'desc_tip'    => true,
                 ),
                 'api_secret' => array(
-                    'title'       => 'Secreto API',
+                    'title'       => 'Secret API',
                     'type'        => 'password',
-                    'description' => 'Ingresa tu secreto de API proporcionado por el servicio de pagos.',
+                    'description' => 'Enter your API secret provided by the payment service.',
                     'default'     => '',
+                    'desc_tip'    => true,
+                ),
+                'domain' => array(
+                    'title'       => 'Domain URL',
+                    'type'        => 'text',
+                    'description' => 'Enter the domain name of your website.',
+                    'default'     => 'http://example.com',
                     'desc_tip'    => true,
                 ),
                 // Puedes agregar más campos aquí según sea necesario
@@ -161,32 +159,31 @@ function init_mi_metodo_pago()
         }
     }
 
-    function agregar_mi_metodo_pago($methods)
+    function add_rewire_holding_payment_gateway($methods)
     {
-        $methods[] = 'WC_Mi_Metodo_Pago';
+        $methods[] = 'WC_RewireHolding_Payment_Gateway';
         return $methods;
     }
 
-    add_filter('woocommerce_payment_gateways', 'agregar_mi_metodo_pago');
+    add_filter('woocommerce_payment_gateways', 'add_rewire_holding_payment_gateway');
 }
 
-add_action('plugins_loaded', 'init_mi_metodo_pago');
+add_action('plugins_loaded', 'init_rewire_holding_payment_gateway');
 
-// Manejar la verificación de pago
 function verificar_pago()
 {
     if (!isset($_GET['verificar_pago'])) {
         return;
     }
 
-    $order_id = $_GET['order']; // Validar y limpiar esto adecuadamente
+    $order_id = $_GET['order']; 
     $order = wc_get_order($order_id);
     $payment_id =
 
-    $api_response = verificar_estado_pago_api($order->get_meta('payment_api_id'));
+        $api_response = verificar_estado_pago_api($order->get_meta('payment_api_id'));
 
     if (is_wp_error($api_response)) {
-	    error_log("error");
+        error_log("error");
         $order->update_status('failed', 'Error in payment API.');
         wc_add_notice(__('The payment was not successful, please contact with your bank', 'text-domain'), 'error');
         wp_redirect(wc_get_checkout_url());
@@ -197,17 +194,14 @@ function verificar_pago()
     $response_code = wp_remote_retrieve_response_code($api_response);
 
 
-    // Comprobar el código de estado HTTP de la respuesta
 
     if ($response_code == 200) {
-        // Si el código es 200, el pago fue exitoso
         $order->payment_complete();
         wp_redirect($order->get_checkout_order_received_url());
         exit;
     } else {
-	    error_log("error");
+        error_log("error");
 
-        // Si el código no es 200, el pago falló
         $order->update_status('failed', 'The payment was not successful');
         wc_add_notice(__('The payment was not successful, please contact with your bank', 'text-domain'), 'error');
         wp_redirect(wc_get_checkout_url());
@@ -218,11 +212,8 @@ function verificar_pago()
 }
 function verificar_estado_pago_api($tx_id)
 {
-    // Aquí deberías configurar la llamada a tu API para verificar el estado del pag
-    //password
 
-
-    $api_url = 'https://api.saurus.com/data/api/v1.0/merchant/topup/paymentgateway/api/polling/'.$tx_id;
+    $api_url = 'https://api.saurus.com/data/api/v1.0/merchant/topup/paymentgateway/api/polling/' . $tx_id;
     error_log($api_url);
     $headers = array(
         'Content-Type' => 'application/json'
@@ -234,7 +225,6 @@ function verificar_estado_pago_api($tx_id)
         'timeout' => 45,
     );
 
-    // Realiza la llamada a la API
     $response = wp_remote_post($api_url, $api_args);
     return $response;
 }
